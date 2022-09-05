1d8764c31dd831d5cf6d4117c775314b060cadd3
Arrafifajri <112527317+Arrafifajri@users.noreply.github.com>
2022-09-05 08:30:25 +0700

    Add files via upload

---
+++ python5-tkj4.py
@@ -0,0 +1,25 @@
+# mengconvert mata uang 
+
+#dolar ke rupiah
+
+print ("mata uang dolar ke rupiah")
+dolar =int(input("dolar = "))
+
+rupiah = dolar * 14.909
+print ("rupiah=", rupiah)
+
+#euro ke yen
+
+print("mata uang euro ke yen")
+uero= int(input("uero = "))
+
+yen = uero * 139.09
+print("yen =", yen)
+
+#won ke rupiah
+
+print ("mata uang won ke rupiah")
+won =int(input("won = "))
+
+rupiah = won * 10.92
+print ("rupiah=", rupiah)
