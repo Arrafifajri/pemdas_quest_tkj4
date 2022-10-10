8ebfe0454f38fac8649c02052785f4f774d8bc1a
Arrafifajri <112527317+Arrafifajri@users.noreply.github.com>
2022-10-09 19:44:43 -0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,18 @@
+#membuat tipe data koleksi
+
+#koleksi_data_str= ["pisang","mangga",'jambu','semangka']
+#koleksi_data_int= [200,300,400,500]
+#print("\nkoleksi data string:",koleksi_data_str)
+#print("\nkoleksi data sinteger:",koleksi_data_int)
+#print("\nkoleksi data mixe:",koleksi_data_mix)
+
+#tugas membuat 3 kumpulan data = nama hewan , nilai uts
+#nama teman kalian
+
+nama_hewan = ['anjing','kucing','kuda','monyet']
+nilai_uts = [75.80,85,70,90,95]
+nama_teman_sebangku =["rifky cepot","hilal aseli","rafi A bocah linux","lulu penuh","abyan roblox"]
+
+print("\nnama hewan-hewan :",nama_hewan)
+print("\nnilai uts :",nilai_uts)
+print("\nnama teman sebangku :",nama_teman_sebangku)
