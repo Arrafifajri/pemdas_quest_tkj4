a933111624d0c9ce0b4f7fe9a11f72772e47782a
Arrafifajri <112527317+Arrafifajri@users.noreply.github.com>
2022-10-12 00:10:33 -0700

    Add files via upload

--- python10-tkj4.py
+++ python10-tkj4.py
@@ -10,9 +10,42 @@
 #nama teman kalian
 
 nama_hewan = ['anjing','kucing','kuda','monyet']
-nilai_uts = [75.80,85,70,90,95]
+nilai_uts = [75,80,85,70,90,95]
 nama_teman_sebangku =["rifky cepot","hilal aseli","rafi A bocah linux","lulu penuh","abyan roblox"]
 
 print("\nnama hewan-hewan :",nama_hewan)
-print("\nnilai uts :",nilai_uts)
-print("\nnama teman sebangku :",nama_teman_sebangku)
+print("nilai uts :",nilai_uts)
+print("cnama teman sebangku :",nama_teman_sebangku)
+
+#tampilkan data koleksi dengan indeks
+
+#data ke 2 nama hewan
+
+print("\ndata ke 2 nama hewan :",nama_hewan[1])
+
+#data pertama nilai uts
+
+print("\ndata nilai pertama =", nilai_uts[0])
+
+#data terakhir nama teman sebangku
+
+print("\ndata terakhir nama teman =", nama_teman_sebangku[4])
+
+#tambah 1 data disetiap koleksi
+
+nama_hewan.append("Kuda nil")
+nilai_uts.append(100)
+nama_teman_sebangku.append("megachan")
+print("\n",nama_teman_sebangku)
+print(nilai_uts)
+print(nama_hewan)
+
+#ubahlah data terakhir nilai uts
+
+nilai_uts[5] =95
+print("\nnilai uts yang telah di rubah =",nilai_uts)
+
+#ubahlah data ke-3 nama hewan
+
+nama_hewan[2] ="hiu"
+print("\nnama hewan setelah di rubah =",nama_hewan)
