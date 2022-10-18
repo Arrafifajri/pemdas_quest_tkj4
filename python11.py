5b1c5c24f313ab7ef805061d2b6a1d74b2c1ba0b
Arrafifajri <112527317+Arrafifajri@users.noreply.github.com>
2022-10-17 19:53:59 -0700

    Add files via upload

---
+++ python11.py
@@ -0,0 +1,18 @@
+# #geberal
+# gender =("laki-laki","perempuan")
+# print(gender)
+# #menggunkanv fungsi tuple, tuple()
+# tuple_name = tuple(["jayabaya","lebton james"])
+# print("\n" ,tuple_name)
+
+# gabungannya = gender + tuple_name
+
+# print("\n",gabungannya)
+
+tuple_1 = tuple ({"naysa","afifah","ira","tiara"})
+tuple_2 = tuple ({"monyet","babi","rusa","anjing"})
+tuple_3 = tuple ({"cepot","hilal Aseli","angga tambal ban"})
+
+print(tuple_1)
+print(tuple_2)
+print(tuple_3)
